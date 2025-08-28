b435ea9e91482acf3914c4f2b9ddd0eaf9bdc81b
Randy <randy@mg-consulting-it-act.com>
2025-08-28 13:56:43 +0300

    Ajouter plus de bouton de contrôle sur le map

--- javascript/map.js
+++ javascript/map.js
@@ -1,14 +1,21 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -18.86898, lng: 47.48806 }, 
-      zoom: 17,
-      mapId: "39da6c84efc23aa8a0ebe1e9", 
+        center: { lat: -18.86898, lng: 47.48806 },
+        fillColor: "#FF0000",
+        fillOpacity: 0.2,
+        strokeWeight: 2,
+        zoom: 17,
+        zoomControl: true,          
+        mapTypeControl: false,      
+        fullscreenControl: true,    
+        streetViewControl: true,    
+        scaleControl: true,        
+        mapId: "39da6c84efc23aa8a0ebe1e9",
     });
-  
+
     const marker = new google.maps.marker.AdvancedMarkerElement({
-      map,
-      position: { lat: -18.86898, lng: 47.48806 }, 
-      title: "My house",
+        map,
+        position: { lat: -18.86898, lng: 47.48806 },
+        title: "My house",
     });
-  }
-  
+}
