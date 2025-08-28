6769cde9daefbcc1421c94a166cdd7316e6b5b74
Randy <randy@mg-consulting-it-act.com>
2025-08-28 11:53:29 +0300

    Ajout de google map pour chez ma maison

---
+++ javascript/map.js
@@ -0,0 +1,14 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -18.86898, lng: 47.48806 }, 
+      zoom: 10,
+      mapId: "39da6c84efc23aa8a0ebe1e9", 
+    });
+  
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+      map,
+      position: { lat: -18.86898, lng: 47.48806 }, 
+      title: "My house",
+    });
+  }
+  
